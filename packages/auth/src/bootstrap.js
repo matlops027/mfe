39330738f76ec0fe449ec8f67d79d48e90ff23f3
Matheus Lopes <matheus.l.alves@hotmail.com>
2023-03-09 18:28:13 -0300

    fix auth prod

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
 
-const mount = (el, { onSignIn, onNavigate, dafaultHistory, initialPath}) => {
-    const history = dafaultHistory || createMemoryHistory({
-        initialEntries:[initialPath]
+// Mount function to start up the app
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
     });
-    if(onNavigate) {
-        history.listen(onNavigate);
-    }
 
-    ReactDOM.render(<App history={history} onSignIn={onSignIn} />, ( el));
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
 
-    return {
-        onParentNavigate({ pathname: nextPathname }) {
-            const { pathname } = history.location;
-            if(pathname !== nextPathname){
-                history.push(nextPathname);
-            }
-        }
-    }
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
+
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
 }
 
-if(process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_auth-dev-root');
-    if(devRoot) {
-        mount(devRoot, { dafaultHistory: createBrowserHistory() });
-    }
-}
-
+// We are running through container
+// and we should export the mount function
 export { mount };
